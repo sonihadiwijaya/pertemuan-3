532cf42edb7d2fafb4c96b695b0da73a2a9dd997
sonihadiwijaya <32594058+sonihadiwijaya@users.noreply.github.com>
2017-10-10 22:54:19 +0700

    Add files via upload

---
+++ prosedursarjana.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+void prosedursekolahdasar ();
+void prosedursekolahmenengahpertama ();
+void prosedursekolahmenengahatas ();
+void prosedurperguruantinggi ();
+
+int main() {
+prosedursekolahdasar ();
+prosedursekolahmenengahpertama ();
+prosedursekolahmenengahatas ();
+prosedurperguruantinggi ();
+return 0;
+}
+
+void prosedursekolahdasar (){
+cout << "1. masuk sd kelas 1" <<endl;
+cout << "2. naik sd kelas 2" <<endl;
+cout << "3. naik sd kelas 3" <<endl;
+cout << "4. naik sd kelas 4 " <<endl;
+cout << "5. naik  sd kelas 5" <<endl;
+cout << "6. naik sd kelas 6" <<endl;
+cout << "7. lulus sekolah dasar" <<endl;
+}
+void prosedursekolahmenengahpertama (){
+cout << "======================================" << endl;
+cout << "8. masuk sekolah menengah pertama (SMP) " <<endl;
+cout << "9. masuk smp kelas 1" <<endl;
+cout << "10. masuk smp kelas 2" <<endl;
+cout << "11. masuk smp kelas 3" <<endl;
+cout << "12. lulus dari SMP" <<endl;
+}
+void prosedursekolahmenengahatas (){
+cout << "======================================" << endl;
+cout << "13. lanjut Sekolah Menengah Atas" <<endl;
+cout << "14. masuk SMA kelas 1" <<endl;
+cout << "15. masuk SMA kelas 2" <<endl;
+cout << "16. masuk SMA kelas 3" <<endl;
+cout << "17. lulus dari Sekolah Menengah Atas" <<endl;
+}
+void prosedurperguruantinggi (){
+cout << "=====================================" << endl;
+cout << "18. melanjutkan perguruan tinggi " <<endl;
+cout << "19. masuk semester 1" <<endl;
+cout << "20. naik semester 2" <<endl;
+cout << "21. naik semester 3" <<endl;
+cout << "22. naik semester 4" <<endl;
+cout << "23. naik semester 5" <<endl;
+cout << "24. naik semester 6" <<endl;
+cout << "25. naik semester 7" <<endl;
+cout << "26. naik semester 8" <<endl;
+cout << "27. melaksanakan sidang" <<endl;
+cout << "28. lulus" <<endl;
+cout << "29. menjadi sarjana " <<endl;
+
+}
